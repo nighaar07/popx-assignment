16df82c6616ffb230b97f3b13c854af6bfa90532
Nighaar Taneem <tnighaar@gmail.com>
2026-06-08 18:07:17 +0530

    Build welcome page

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,5 +1,40 @@
+import { useNavigate } from "react-router-dom";
+import "../styles/welcome.css";
+
 function Welcome() {
-  return <div>Welcome Page</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="mobile-container">
+      <div className="welcome-content">
+        <div>
+          <h1>Welcome to PopX</h1>
+
+          <p>
+            Lorem ipsum dolor sit amet,
+            <br />
+            consectetur adipiscing elit,
+          </p>
+        </div>
+
+        <div className="button-group">
+          <button
+            className="create-btn"
+            onClick={() => navigate("/signup")}
+          >
+            Create Account
+          </button>
+
+          <button
+            className="login-btn"
+            onClick={() => navigate("/login")}
+          >
+            Already Registered? Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Welcome;

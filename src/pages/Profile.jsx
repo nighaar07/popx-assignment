eb2eb22d25b62256e2776a65a23d82d67af64a9e
Nighaar Taneem <tnighaar@gmail.com>
2026-06-08 19:17:31 +0530

    Built profile page and user data display

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,54 @@
+import { useEffect, useState } from "react";
+import "../styles/profile.css";
+
 function Profile() {
-  return <div>Profile Page</div>;
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storedUser = JSON.parse(
+      localStorage.getItem("user")
+    );
+
+    setUser(storedUser);
+  }, []);
+
+  return (
+    <div className="mobile-container">
+      <div className="profile-header">
+        Account Settings
+      </div>
+
+      {user && (
+        <div className="profile-content">
+
+          <div className="profile-info">
+
+            <div className="avatar">
+              <img
+                src="https://i.pravatar.cc/100"
+                alt="profile"
+              />
+            </div>
+
+            <div>
+              <h3>{user.fullName}</h3>
+              <p>{user.email}</p>
+            </div>
+
+          </div>
+
+          <div className="profile-description">
+            Lorem Ipsum Dolor Sit Amet,
+            Consetetur Sadipscing Elitr, Sed Diam
+            Nonumy Eirmod Tempor Invidunt Ut
+            Labore Et Dolore Magna Aliquyam Erat,
+            Sed Diam.
+          </div>
+
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Profile;
